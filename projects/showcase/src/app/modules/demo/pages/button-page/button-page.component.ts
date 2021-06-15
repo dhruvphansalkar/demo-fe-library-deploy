ea91d3197f412c9f8c3c7ba45e829303a8769cb0
dphansal <dhruv.phansalkar@capgemini.com>
2021-06-15 16:01:33 +0530

    fixed bugs with button styles

--- projects/showcase/src/app/modules/demo/pages/button-page/button-page.component.ts
+++ projects/showcase/src/app/modules/demo/pages/button-page/button-page.component.ts
@@ -7,14 +7,14 @@ import { Component, OnInit, AfterViewInit, ChangeDetectorRef } from '@angular/co
 })
 export class ButtonPageComponent implements OnInit {
 
-  buttonCode = `<button class="btn btn-primary-demo m-1">Enabled primary button</button>
-  <button disabled class="btn btn-primary-demo m-1">Disabled primary button</button>
-  <button class="btn btn-success-demo m-1">Enabled success button</button>
-  <button disabled class="btn btn-success-demo m-1">Disabled success button</button>
-  <button class="btn btn-outline-primary-demo m-1">Outline Primary</button>
-  <button disabled class="btn btn-outline-primary-demo m-1">Disabled Outline Primary </button>
-  <button class="btn btn-link-demo m-1"><i class="fa fa-sign-in"></i> IMPORTA DA XML</button>
-  <button disabled class="btn btn-link-demo m-1"><i class="fa fa-sign-in"></i> IMPORTA DA XML</button>`;
+  buttonCode = `<button class=" btn-primary-demo m-1">Enabled primary button</button>
+  <button disabled class=" btn-primary-demo m-1">Disabled primary button</button>
+  <button class=" btn-success-demo m-1">Enabled success button</button>
+  <button disabled class=" btn-success-demo m-1">Disabled success button</button>
+  <button class=" btn-outline-primary-demo m-1">Outline Primary</button>
+  <button disabled class=" btn-outline-primary-demo m-1">Disabled Outline Primary </button>
+  <button class=" btn-link-demo m-1"><i class="fa fa-sign-in"></i> IMPORTA DA XML</button>
+  <button disabled class=" btn-link-demo m-1"><i class="fa fa-sign-in"></i> IMPORTA DA XML</button>`;
 
   constructor() { }
 
